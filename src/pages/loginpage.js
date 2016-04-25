a4eaf97d883a84508d41a42df30c49f5019e60b1
dmv <dima.voronov2008@gmail.com>
2016-04-25 13:28:19 +0300

    Revert "add loginPage"
    This reverts commit 8acd393bc5e331ba1df2ece1714fc2efbb8f26ec.

--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -1,17 +1,1 @@
-	console.log("in module LoginPage");
-
-	exports.enterUserName = function(name) {
-		// console.log(name);
-		return this.elementByCss("#username input").type(name);
-	}
-
-	exports.enterUserPassword = function(password) {
-		return this.elementByCss("input[type='password']").type(password);
-	}
-
-	exports.login = function() {
-		return this.elementByCss("button").click();
-	}
-	
-
-	console.log("after module login page");
+var name = "#username input";
